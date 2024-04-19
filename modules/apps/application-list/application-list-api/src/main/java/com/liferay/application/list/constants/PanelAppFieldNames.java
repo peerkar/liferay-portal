6ab91a3a7839202dda7ecce490cf6c6ec4330717
Petteri Karttunen <petteri.karttunen@liferay.com>
2024-04-19 11:22:43 +0300

    LPD-22489 application-list-api: Add field name constants

---
+++ modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/constants/PanelAppFieldNames.java
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.application.list.constants;
+
+/**
+ * @author Petteri Karttunen
+ */
+public class PanelAppFieldNames {
+
+	public static final String PANEL_APP_CATEGORY = "panelAppCategory";
+
+	public static final String PANEL_APP_CATEGORY_KEY = "panelAppCategoryKey";
+
+	public static final String PANEL_APP_KEY = "panelAppKey";
+
+	public static final String PANEL_APP_ROOT_CATEGORY = "panelAppRootCategory";
+
+	public static final String PORTLET_ICON = "portletIcon";
+
+	public static final String PORTLET_ID = "portletId";
+
+}
