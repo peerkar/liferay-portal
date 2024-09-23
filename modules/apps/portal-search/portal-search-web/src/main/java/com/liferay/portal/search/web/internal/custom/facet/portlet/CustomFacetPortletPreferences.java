80f8c45551f67132bc9b365cd237ca9992d4a97a
Petteri Karttunen <petteri.karttunen@liferay.com>
2024-09-23 23:50:28 +0300

    LPD-36594 portal-search-web: Migrate date facet preferences

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/custom/facet/portlet/CustomFacetPortletPreferences.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/custom/facet/portlet/CustomFacetPortletPreferences.java
@@ -5,6 +5,8 @@
 
 package com.liferay.portal.search.web.internal.custom.facet.portlet;
 
+import com.liferay.portal.kernel.json.JSONArray;
+
 /**
  * @author Wade Cao
  */
@@ -12,6 +14,9 @@ public interface CustomFacetPortletPreferences {
 
 	public static final String PREFERENCE_KEY_AGGREGATION_FIELD =
 		"aggregationField";
+
+	public static final String PREFERENCE_KEY_AGGREGATION_TYPE =
+		"aggregationType";
 
 	public static final String PREFERENCE_KEY_CUSTOM_HEADING = "customHeading";
 
@@ -30,7 +35,11 @@ public interface CustomFacetPortletPreferences {
 
 	public static final String PREFERENCE_KEY_PARAMETER_NAME = "parameterName";
 
+	public static final String PREFERENCE_KEY_RANGES = "ranges";
+
 	public String getAggregationField();
+
+	public String getAggregationType();
 
 	public String getCustomHeading();
 
@@ -43,6 +52,10 @@ public interface CustomFacetPortletPreferences {
 	public String getOrder();
 
 	public String getParameterName();
+
+	public JSONArray getRangesJSONArray();
+
+	public String getRangesString();
 
 	public boolean isFrequenciesVisible();
 
