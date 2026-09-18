b07a603bcf5cfda39fc9f50bd4d7c35fa83b30a8
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-09-18 13:04:54 +0300

    LPD-101772 mcp-server-rest-impl: Add exception mapper

---
+++ modules/apps/mcp/mcp-server-rest-impl/src/main/java/com/liferay/mcp/server/rest/internal/jaxrs/exception/mapper/ToolSearchUnavailableExceptionMapper.java
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.mcp.server.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.mcp.server.rest.internal.search.exception.ToolSearchUnavailableException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Petteri Karttunen
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.MCP.Server)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.MCP.Server.ToolSearchUnavailableExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class ToolSearchUnavailableExceptionMapper
+	extends BaseExceptionMapper<ToolSearchUnavailableException> {
+
+	@Override
+	protected Problem getProblem(
+		ToolSearchUnavailableException toolSearchUnavailableException) {
+
+		return new Problem(
+			Response.Status.SERVICE_UNAVAILABLE,
+			toolSearchUnavailableException.getMessage());
+	}
+
+}
